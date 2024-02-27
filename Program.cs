12e01a43e4e0311245f231d252df09d54be44bf5
alyak <alyak@DESKTOP-3MDJHC4>
2024-02-27 13:26:10 +0300

    0.3#kkk0,4dd

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace ZADANIE_10
 {
@@ -11,11 +9,11 @@ namespace ZADANIE_10
             bool exit = false;
             while (!exit)
             {
-                VvodFirura();
+                InputFigure();
 
-                Console.WriteLine("Введите 'exit' для выхода:");
-                string exitword = Console.ReadLine();
-                if (exitword == "exit")
+                Console.WriteLine("Print 'exit' for end:");
+                string exitWord = Console.ReadLine();
+                if (exitWord == "exit")
                     exit = true;
             }
         }
@@ -23,6 +21,7 @@ namespace ZADANIE_10
         // Функция рисования шахматной доски.
         static void DrawChessboard(int oneX, int oneY, int twoX, int twoY)
         {
+            Console.WriteLine();
             Console.WriteLine("  a b c d e f g h");
             for (int i = 8; i >= 1; i--)
             {
@@ -32,79 +31,107 @@ namespace ZADANIE_10
                     if ((j == oneX && i == oneY) || (j == twoX && i == twoY))
                         Console.Write((j == oneX && i == oneY) ? "Y " : "B ");
                     else
-                        Console.Write((j + i) % 2 == 0 ? "- " : "/ ");
+                        Console.Write((j + i) % 2 == 0 ? "- " : "  ");
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
         }
 
-        // Функция ввода и проверки данных.
-        static void VvodFirura()
+        // Ввод и последующие функции.
+        static void InputFigure()
         {
-            bool vvod_reght;
+            bool inputCorrect;
             do
             {
-                Console.Write("Введите наименование фигуры " +
-                    "и ее начальную координатy" + Environment.NewLine +
-                    "Например: ферзь b3: ");
-                string[] coordinat = Console.ReadLine()?.Split(' ');
+                Console.Write("Enter the figure name and its position" + Environment.NewLine +
+                    "1. rook" + Environment.NewLine + "2. knight" +
+                    Environment.NewLine + "3. bishop" +
+                    Environment.NewLine + "4. queen" +
+                    Environment.NewLine + "5. king" +
+                    Environment.NewLine + "Example: queen b3: ");
+                string[] coordinates = Console.ReadLine()?.Split(' ');
 
-                vvod_reght = coordinat?.Length == 2
-                    && CorrectPosition(coordinat[1])
-                    && CorrectFigura(coordinat[0]);
+                inputCorrect = coordinates?.Length == 2
+                               && CorrectPosition(coordinates[1])
+                               && CorrectFigure(coordinates[0]);
 
-                if (vvod_reght)
-                    CoordinatsEqval(coordinat[1], coordinat[0]);
+                if (inputCorrect)
+                    CompareCoordinates(coordinates[1], coordinates[0]);
 
-            } while (!vvod_reght);
+            } while (!inputCorrect);
         }
 
-        // Функция сравнения координат 1 и 2 позиции.
-        static void CoordinatsEqval(string first, string figura)
+        // Сравнение 1 и 2 позиции фигур
+        static void CompareCoordinates(string first, string figure)
         {
-            Random randon = new Random();
+            Random random = new Random();
             int firstX = first[0] - 'a' + 1;
             int firstY = first[1] - '0';
-            int secondX = randon.Next('a', 'h') - 'a' + 1;
-            int secondY = randon.Next(1, 9);
+            int secondX = random.Next('a', 'h');
+            int secondY = random.Next(1, 9);
+
+            char indexX = ((char)secondX);
+            secondX = secondX - 'a' + 1;
 
             DrawChessboard(firstX, firstY, secondX, secondY);
 
-            switch(figura)
+            int xDiff = Math.Abs(firstX - secondX);
+            int yDiff = Math.Abs(firstY - secondY);
+
+            bool isValid = false;
+
+            switch (figure)
             {
-                case "ладья":
+                case "rook":
+                    isValid = secondX == firstX || secondY == firstY;
                     break;
-                case "конь":
+                case "knight":
+                    isValid = (xDiff == 1 && yDiff == 2) || (xDiff == 2 && yDiff == 1);
                     break;
-                case "слон":
+                case "bishop":
+                    isValid = xDiff == yDiff;
                     break;
-                case "ферзь":
+                case "queen":
+                    isValid = (xDiff == yDiff) || (secondX == firstX || secondY == firstY);
                     break;
-                case "король":
+                case "king":
+                    isValid = xDiff <= 1 && yDiff <= 1;
                     break;
             }
-            
+
+            ConsoleWrite(first, figure, $"{indexX}{secondY}", isValid);
         }
 
-        // Функция проверки на правильность ввода.
+        // Вывод итого решения на консоль.
+        static void ConsoleWrite(string xy, string figure, string x2y2, bool isValid)
+        {
+            if (isValid)
+                Console.WriteLine($"If the {figure} is at {xy}, it can move to {x2y2} in one move.");
+            else
+                Console.WriteLine($"If the {figure} is at {xy}, it cannot kill {x2y2}.");
+        }
+
+        // Функция проверки вводных данных (кооридинат).
         static bool CorrectPosition(string position)
         {
             if (position.Length != 2)
                 return false;
 
-            char bokva = position[0];
-            char numer = position[1];
+            char letter = position[0];
+            char number = position[1];
 
-            return bokva >= 'a' && bokva <= 'h' && numer >= '1' && numer <= '8';
+            return letter >= 'a' && letter <= 'h' && number >= '1' && number <= '8';
         }
 
-        static bool CorrectFigura(string figura)
+        // Функция проверки вводных данных (фигура)
+        static bool CorrectFigure(string figure)
         {
-            string[] figurs = { "ладья", "конь",
-                "слон", "ферзь", "король" };
-            for (int i = 0; i < figurs.Length; i++)
+            string[] figures = { "rook", "knight", "bishop", "queen", "king" };
+
+            foreach (string fig in figures)
             {
-                if (figurs[i] == figura)
+                if (fig == figure)
                     return true;
             }
             return false;
